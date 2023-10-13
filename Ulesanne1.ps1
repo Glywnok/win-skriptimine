3a505b533354512d21bf076de6308e9c4a49a134
Glywnok <glywnok@gmail.com>
2023-10-13 11:41:16 +0300

    1 ja 2 ulesanne

---
+++ Ulesanne1.ps1
@@ -0,0 +1,26 @@
+﻿#Ulesanne 1
+
+#Teeme andmed ja kusime need kasutajast
+#$Kasutajanimi = Read-Host -Prompt 'Sisestage Kasutaja nimet: '
+
+
+
+$Parool = ConvertTo-SecureString "Parool1" -AsPlainText -Force
+$Name = Read-Host -Prompt 'Sisestage oma nimet: '
+$LastName = Read-Host -Prompt 'Sisestage oma prenimet: '
+$KontoKirjeldus = Read-Host -Prompt 'Keda/Mida see konto on/teeb: '
+
+#Nuud teeme need Lower Case-ina
+$nimi = "$Name + $LastName"
+$kirjeldus = $KontoKirjeldus.ToLower()
+$Kasutajanimi = $Name.ToLower() + "." + $LastName.ToLower()
+
+#Nuud vaatame, kas on uldse kasutaja selle nimega
+$UserExists = Get-LocalUser -Name $Kasutajanimi -ErrorAction SilentlyContinue
+
+if ($UserExists) {
+    Write-Output "On juba kasutaja nimega $Kasutajanimi"
+    Exit
+}
+
+New-LocalUser -Name "$Kasutajanimi" -Password $Parool -Fullname "$nimi" -Description "$kirjeldus"
---
+++ Ulesanne2.ps1
@@ -0,0 +1,26 @@
+﻿#Ulesanne 2
+
+#Teeme andmed ja kusime need kasutajast
+#$Kasutajanimi = Read-Host -Prompt 'Sisestage Kasutaja nimet: '
+
+
+
+
+$Name = Read-Host -Prompt 'Sisestage oma nimet: '
+$LastName = Read-Host -Prompt 'Sisestage oma prenimet: '
+$KontoKirjeldus = Read-Host -Prompt 'Keda/Mida see konto on/teeb: '
+
+#Nuud teeme need Lower Case-ina
+$nimi = "$Name + $LastName"
+$kirjeldus = $KontoKirjeldus.ToLower()
+$Kasutajanimi = $Name.ToLower() + "." + $LastName.ToLower()
+
+#Nuud vaatame, kas on uldse kasutaja selle nimega
+$UserExists = Get-LocalUser -Name $Kasutajanimi -ErrorAction SilentlyContinue
+
+if ($UserExists) {
+    Write-Output "On juba kasutaja nimega $Kasutajanimi"
+    Remove-LocalUser $Kasutajanimi
+} else {
+    Write-Output "Kasutajat sellisega nimega ei ole"
+}
